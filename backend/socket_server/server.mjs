ebc2f5ac04b19a92b6a9a8f721f840d50b75ff80
lukasVirtual <gerlinskilukas@gmail.com>
2022-10-15 16:31:16 +0200

    redis integration

--- backend/socket_server/server.mjs
+++ backend/socket_server/server.mjs
@@ -1,33 +1,46 @@
 import express from "express"
 import http from "http"
 import { Server } from "socket.io"
+// import { createAdapter } from "@socket.io/redis-adapter";
+import redis from "redis"
 const app = express()
 const server = http.createServer(app)
+const client = redis.createClient({ url: "redis://localhost:6379"})
+await client.connect()
 const io = new Server(server, { cors: {
     origin: "http://127.0.0.1:3000",
     methods: ["GET", "POST"]
 }})
 
+// io.adapter(createAdapter(client, client.duplicate()))
+
 app.get('/', (req, res) => {
     res.send("<h1>Working</h1>")
 })
 
-const socketUsers = new Map()
+// const socketUsers = new Map()
 io.on('connection', (socket) => {
-    socket.on('login', (user) => {
+    socket.on('login', async (user) => {
         console.log(`User: ${user.user_name} with id ${user.user_id} socket id ${socket.id} connected`)
-        socketUsers.set(user.user_name, socket.id)
+        // socketUsers.set(user.user_name, socket.id)
+        await client.set(user.user_name, socket.id, function(err) { if (err) throw err })
     })
 
-    socket.on('logout', (user) => {
-        console.log(`User: ${user.user_name} with id ${user.user_id} disconnected`)
-        if (socketUsers.get(user.user_name)) socketUsers.delete(user.user_name)
+    socket.on('logout', async (user) => {
+        const exists = await client.exists(user.user_name)
+        if (exists === 1) {
+            console.log(`User: ${user.user_name} with id ${user.user_id} disconnected`)
+            await client.del(user.user_name)
+        } else console.log("no one connected")
+
+        // if (socketUsers.get(user.user_name)) socketUsers.delete(user.user_name)
     })
     
-    socket.on('submit', (data, sendTo) => {
+    socket.on('submit', async (data, sendTo) => {
         console.log("data", data)
         //Need to be done
-        const reciever = socketUsers.get(sendTo)
+        // const reciever = socketUsers.get(sendTo)
+        const reciever = await client.get(sendTo)
         if (reciever) {
             console.log(reciever)
             /* TODO
@@ -42,6 +55,7 @@ io.on('connection', (socket) => {
             //io.to(reciever).emit("test", data)
  
             io.emit("test", data)
+            // io.to(reciever).emit("test", data)
             console.debug("message sent")
         } else {
             console.error("error: ", reciever)
